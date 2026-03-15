553bd5bcfb2c1335be5090bfecbd6a467236718d
drLacheheb <yacinolach@gmail.com>
2026-03-15 12:35:59 +0100

    Remove unused dataset-related scripts and metadata files.

--- utils.py
+++ utils.py
@@ -1,18 +1,13 @@
 import math
-from typing import Optional
+from typing import Optional, Callable
+import torch
+from torch import Tensor
 
 
 def make_divisible(v: float, divisor: int, min_value: Optional[int] = None) -> int:
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    """
     if min_value is None:
         min_value = divisor
     new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
     if new_v < 0.9 * v:
         new_v += divisor
     return new_v
@@ -21,3 +16,14 @@ def make_divisible(v: float, divisor: int, min_value: Optional[int] = None) -> i
 def cnn_out_size(in_size, padding, dilation, kernel, stride):
     s = in_size + 2 * padding - dilation * (kernel - 1) - 1
     return math.floor(s / stride + 1)
+
+
+def collapse_dim(x: Tensor, dim: int, mode: str = "pool", pool_fn:  Callable[[Tensor, int], Tensor] = torch.mean,
+                 combine_dim: int = None):
+    if mode == "combine":
+        s = list(x.size())
+        s[combine_dim] *= dim
+        s[dim] //= dim
+        return x.view(s)
+
+    return pool_fn(x, dim)
